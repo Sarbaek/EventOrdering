c0d9aaf13fcfc89f8986aff77eef9b25debdd280
Lars Skov Sarbæk <lsk@delegate.dk>
2024-10-08 22:11:21 +0200

    Initial commit

---
+++ EventHandlerTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventOrdering
+{
+    public static class EventHandlerTask
+    {
+        public static async Task HandleEvents(IAccountEventHandler accountEventHandler, string podName)
+        {
+            await Task.Delay(1000);
+            Console.WriteLine($"Handling events for pod {podName}");
+        }
+    }
+}
